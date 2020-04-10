1249cb0394736562ecda88f70ed7d4a1121daa55
Wilson Calil <wilson.calil@outlook.com>
2020-04-09 21:16:35 -0500

    new model seeds.sql.

---
+++ models/seeds.sql
@@ -0,0 +1,12 @@
+/* Seeds for SQL table. We haven't discussed this type of file yet */
+USE user_credentials;
+
+-- /* Insert 3 Rows into your new table */
+-- INSERT INTO users (email)
+-- VALUES ("wcalil@gmail.com");
+
+-- INSERT INTO users (password)
+-- VALUES ("123");
+
+INSERT INTO interests (interest)
+VALUES ("Play Soccer"), ("Go to the museum");
